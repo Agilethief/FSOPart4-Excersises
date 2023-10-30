d8c7de345737963f85174847fabeff58db6850fb
Agilethief <tbermanseder@gmail.com>
2023-10-30 20:48:20 +1100

    Setting up for 4.15

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    minlength: 3,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+    },
+  ],
+});
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject.passwordHash;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
